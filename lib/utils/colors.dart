4fb56cf188ee4ef7ab4d8dacf92c65a909fbbf6a
ousseeema <oussama.ferchichi01@gmail.com>
2023-12-16 22:41:53 +0100

    colors and animation .. colorrs also

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,4 +4,11 @@ class colors{
 
   static Color mainColor = Colors.black;
   static Color secondsColor = Colors.cyan;
+  static Color? buttoncolor = Colors.red[100];
+  static Color? backgroundColor = Colors.blue[50];
+  static Color? secondbackgroundColor = Colors.blue[300];
+  static Color? maintextcolor  = Colors.white54;
+  static Color? buttontextcolor = Colors.white;
+  static Color? bigtextcolor = Colors.black;
+
 }
--- lib/view/splach_screen/splachScreen.dart
+++ lib/view/splach_screen/splachScreen.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:job_app/utils/colors.dart';
+import 'package:job_app/utils/demonstion.dart';
+import 'package:job_app/view/home/homepage.dart';
 
 class splachscreen extends StatefulWidget {
   const splachscreen({super.key});
@@ -10,49 +13,50 @@ class splachscreen extends StatefulWidget {
   State<splachscreen> createState() => _splachscreenState();
 }
 
-class _splachscreenState extends State<splachscreen> with TickerProviderStateMixin{
-  late  AnimationController controller ;
+class _splachscreenState extends State<splachscreen>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
   late Animation<double> animation;
-  Future<void> loadresources()async{
-    // loading ressources that we need in ower application 
+  Future<void> loadresources() async {
+    // loading ressources that we need in ower application
     // loading list of cars and list off available cars
-
   }
 
   @override
   void initState() {
     // TODO: implement initState
-      loadresources();
-      controller = AnimationController(vsync: this,duration: const Duration(seconds: 4)).forward() as AnimationController ;
-     animation = CurvedAnimation(parent: controller, curve: Curves.elasticIn);
-     Timer(Duration(seconds: 5), () {
-      // Get.toNamed to home screen 
+    loadresources();
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 4))
+            .forward() as AnimationController;
+    animation = CurvedAnimation(parent: controller, curve: Curves.elasticIn);
+    Timer(const Duration(seconds: 5), () {
+      // Get.toNamed to home screen
+      Get.to(()=>const HomePage());
+    });
 
-      });
-  
     super.initState();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return   Scaffold( 
+    return Scaffold(
       backgroundColor: colors.mainColor,
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ScaleTransition(
             scale: animation,
-            child: const  Center(
-              child: Image(image: AssetImage("images/splach.png")),
-
+            child: Center(
+              child: Image(
+                image: const AssetImage("images/splachscreen.png"),
+                width: dimensions.splachwidth,
+                height: dimensions.splachheight,
+              ),
             ),
-            )
+          )
         ],
       ),
-
     );
   }
 }
