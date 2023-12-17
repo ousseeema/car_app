aa0218f1c0d9e6d3fc0029a5a6fa5bace35a0d63
ousseeema <oussama.ferchichi01@gmail.com>
2023-12-17 22:10:31 +0100

    costum conatiner

---
+++ lib/utils/costumcontainer.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+import 'package:job_app/utils/colors.dart';
+import 'package:job_app/utils/demonstion.dart';
+
+class costumcontainer extends StatefulWidget {
+  String name ; 
+  String rating;
+  String rank;
+  String location;
+  String imageUrl;
+  String tags;
+   costumcontainer({super.key,required this.name,required this.rating,required this.location,required this.imageUrl,required this.tags, required this.rank});
+
+  @override
+  State<costumcontainer> createState() => _costumcontainerState();
+}
+
+class _costumcontainerState extends State<costumcontainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: dimensions.height10*20,
+      width:  dimensions.width10*20,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(dimensions.radius20)
+
+        ),
+
+        child: Stack(
+            children: [
+              Positioned(
+                //! the image of the rent company car or the logo
+                child: Container(
+                  height:dimensions.height10*11 ,
+                  width: dimensions.width10*20,
+                  decoration: BoxDecoration(
+                    /*image: DecorationImage(
+                      image: NetworkImage(widget.imageUrl),
+                      fit: BoxFit.cover
+                    ),*/
+                    borderRadius: BorderRadius.circular(dimensions.radius20)
+
+                  ),
+                )),
+                // ! hedhy conatiiner besh nhot fiha rating and the name of the company and tags 
+              Positioned( 
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: colors.secondsColor,
+                    borderRadius: BorderRadius.circular(dimensions.radius20)
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Text(widget.name)),
+                             Expanded(
+                            flex: 2,
+                            child: Text(widget.tags)),
+                        ],
+
+                       
+
+                      ),
+                       Row(
+                        children: [
+                         
+                        ],
+                       )
+
+                    ],
+                  ),
+
+                ),
+
+              )
+
+
+
+            ],
+        ),
+
+        
+
+    );
+  }
+}
--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:job_app/utils/colors.dart';
+import 'package:job_app/utils/constante.dart';
+import 'package:job_app/utils/demonstion.dart';
 
 class homePage extends StatefulWidget {
   const homePage({super.key});
@@ -13,7 +15,47 @@ class _homePageState extends State<homePage> {
   Widget build(BuildContext context) {
      return  Scaffold(
       backgroundColor: colors.backgroundColor!,  
-      body: const Center(child: Text('home page '),),
+      body:  SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(dimensions.LRpadmarg20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                 const Expanded(
+                  flex: 4,
+                  child: Text("Hi there ")) ,
+                 Expanded(
+                  //!
+                  //! besh tji fiha el taswira mtaa user mtaana 
+                  child: Container(
+                    height: dimensions.height20 *2.5,
+                    
+                    decoration:  BoxDecoration(
+                      color: colors.buttontextcolor,
+                      borderRadius: BorderRadius.circular(dimensions.radius20)
+                    ),
+                  ),
+                  )
+          
+              ],),
+
+              SizedBox(height: dimensions.height20,),
+
+             ListView.builder(
+              itemCount: 5,
+              itemBuilder:(_ , index){
+                return costumContainer()
+              }
+               )
+          
+          
+          
+          
+            ],
+          ),
+        ),
+      ),
     );
   }
   }
